e0e22ef44fb62f09b1bbedd7ca47297617736029
gunb0s <bys20411@gmail.com>
2022-05-06 11:51:02 +0900

    append navigationbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import Home from "./pages/Home";
+import styled from "styled-components";
+import Navigation from "./components/Navigation";
+
+const Rootdiv = styled.main`
+  display: flex;
+  flex-direction: column;
+`;
 
 const App = () => {
   return (
-    <div>
+    <Rootdiv>
+      <Navigation />
       <Home />
-    </div>
+    </Rootdiv>
   );
 };
 
--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,7 +1,94 @@
 import React from "react";
+import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+const NavBarWrapper = styled.nav`
+  background-color: var(--main-theme);
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 79px;
+  width: 100%;
+  padding: 1rem 1.25rem 1rem 1.25rem;
+
+  box-shadow: 0px 4px 9px 0px rgba(0, 0, 0, 0.75);
+  user-select: none;
+`;
+
+const NavBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+`;
+
+const LogoWrapper = styled.div`
+  font-size: 2rem;
+  cursor: pointer;
+`;
+
+const Navs = styled.ul`
+  display: flex;
+`;
+
+const Nav = styled.li`
+  margin-right: 3rem;
+  font-size: var(--nav-font-size);
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+
+  &: last-child {
+    margin-right: 0;
+  }
+
+  @media (max-width: 1000px) {
+    display: none;
+  }
+`;
+
+const Login = styled.div`
+  cursor: pointer;
+  @media (max-width: 1000px) {
+    display: none;
+  }
+`;
+
+const Menu = styled.div`
+  display: none;
+  @media (max-width: 1000px) {
+    display: flex;
+    align-itmes: center;
+    gap: 1rem;
+    cursor: pointer;
+    font-size: var(--nav-font-size);
+  }
+`;
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <NavBarWrapper>
+      <NavBar>
+        <LogoWrapper>LFG</LogoWrapper>
+        <Navs>
+          <Nav>LOANS</Nav>
+          <Nav>TRADES</Nav>
+          <Nav>TRADE APP</Nav>
+          <Nav>STAKE</Nav>
+          <Nav>
+            <KeyboardArrowDownIcon />
+            MORE
+          </Nav>
+        </Navs>
+        <Login>Login</Login>
+        <Menu>
+          Menu
+          <MenuIcon />
+        </Menu>
+      </NavBar>
+    </NavBarWrapper>
+  );
 };
 
 export default Navigation;
