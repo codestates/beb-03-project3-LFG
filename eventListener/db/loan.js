a1c607b325cff2540c62f4895a17760506a73d38
mango7loco <do7my7best@gmail.com>
2022-05-18 13:04:53 +0900

    Update: database schema Update
    sync with front-end format

--- eventListener/db/loan.js
+++ eventListener/db/loan.js
@@ -4,20 +4,20 @@ const loanSchema = new mongoose.Schema(
   {
     debtor: String,
     creditor: String,
-    duration: String,
+    period: Number,
     amount: String,
-    interestAmount: String,
-    tokenId: String,
+    rateAmount: String,
+    tokenId: Number,
     nftAddress: String,
     loanAddress: String,
-    projectTitle: String,
+    projectName: String,
     team: String,
     tokenURI: String,
-    startTime: { type: Date, default: null },
-    endTime: { type: Date, default: null },
-    paidBackTime: { type: Date, default: null },
+    startAt: { type: Number, default: null },
+    endAt: { type: Number, default: null },
+    paidBackTime: { type: Number, default: null },
     paidBackAmount: { type: String, default: null },
-    status: String, // created, canceled, funded, paidBack, defaulted 대문자 // (status === FUNDED && endTime < nowTime) => 채권자가 아직 담보물을 안 찾아간, 지불기한은 끝난 상태
+    state: String, // created, canceled, funded, paidBack, defaulted 대문자 // (status === FUNDED && endTime < nowTime) => 채권자가 아직 담보물을 안 찾아간, 지불기한은 끝난 상태
   },
   { timestamps: true }
 );
