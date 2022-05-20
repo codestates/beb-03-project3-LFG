32fef3abedf5c21476d507fcc3be98bf1f50c546
gunb0s <bys20411@gmail.com>
2022-05-21 03:26:35 +0900

    Update: .gitignore

--- server/dist/db/trade.js
+++ server/dist/db/trade.js
@@ -2,12 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Trade = void 0;
 var mongoose = require("mongoose");
+var NFTSchema = new mongoose.Schema({
+    tokenURI: String,
+    tokenId: Number,
+    nftAddress: String,
+    team: String,
+    projectName: String,
+}, { _id: false });
 var tradeSchema = new mongoose.Schema({
     tradeId: Number,
     offerAddress: String,
     respondAddress: String,
-    offerNFTList: [String],
-    respondNFTList: [String],
+    offerNFTList: [NFTSchema],
+    respondNFTList: [NFTSchema],
     offerPaidKlay: String,
     respondPaidKlay: String,
     status: String, // CREATED, CANCELLED, FINISHED
