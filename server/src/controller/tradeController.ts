caf8aaeb4e12c6d9b04f4a7c4264c5b6815515b2
mango7loco <do7my7best@gmail.com>
2022-05-20 16:12:48 +0900

    Feat: p2p trade web server api implementation
    function: trade/offer, trade/respond

--- server/src/controller/tradeController.ts
+++ server/src/controller/tradeController.ts
@@ -1,5 +1,17 @@
-export const getTrades = async (req, res, next) => {
-  console.log('getTrades');
-  res.status(200).json({ message: 'succeed' });
-  return;
+import { Trade } from '../db/trade';
+
+export const offerTrades = async (req, res, next) => {
+  const { userAddress } = req.body;
+  const offerList = await Trade.find({ offerAddress: userAddress });
+
+  console.log('offerTrades', offerList);
+  res.status(200).json({ message: 'succeed', offerList: offerList });
+};
+
+export const respondTrades = async (req, res, next) => {
+  const { userAddress } = req.body;
+  const respondList = await Trade.find({ respondAddress: userAddress });
+
+  console.log('respondTrades', respondList);
+  res.status(200).json({ message: 'succeed', respondList: respondList });
 };
