f19995fa2af0f983d471a6b8f7d36503bbe1dc10
mango7loco <96699299+mango7loco@users.noreply.github.com>
2022-05-20 15:48:08 +0900

    Update myPageController.ts

--- server/src/controller/myPageController.ts
+++ server/src/controller/myPageController.ts
@@ -1,21 +1,21 @@
-import { NftList } from '../db/nftList';
+import { NftList } from '../db/nftlist';
 import { getNFT } from '../utils/kas';
 
 export const getWhiteListNFT = async (req, res, next) => {
-  // 이전에 nftList 불러와서
-  // for 문 돌리면선
-  // kas로 만든 모듈 이용해서 해당 주소가 소유한 NFT 정보를 불러옴
-  // 그 다음에 담아서 응답하면 됨
   const { userAddress } = req.body;
-
   let myNftList = [];
+
   const whiteLists = await NftList.find({});
   for (const whiteList of whiteLists) {
-    const tempList = await getNFT(whiteList.nftCA, userAddress);
-    tempList.map((nft) => (nft['nftCA'] = whiteList.nftCA));
-    myNftList = myNftList.concat(tempList);
+    const tempList = await getNFT(whiteList.nftAddress, userAddress);
+    if (tempList) {
+      tempList.map((nft) => {
+        nft['projectName'] = whiteList.projectName;
+        nft['team'] = whiteList.team;
+        nft['nftAddress'] = whiteList.nftAddress;
+      });
+      myNftList = myNftList.concat(tempList);
+    }
   }
-  console.log(myNftList);
-  console.log('getWhiteListNFT');
-  res.status(200).json({ message: 'succeed' });
+  res.status(200).json({ message: 'succeed', myNftList });
 };
